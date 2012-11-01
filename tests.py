ea5f79370bf9c7669faaf567e0970a5a1f24f3fb
Mykyta Khliestov <khlestovnikita@gmail.com>
2012-11-01 03:07:36 +0200

    added model and corrected test to test template used. added /index view

--- tests.py
+++ tests.py
@@ -4,25 +4,27 @@ from notes.models import Note
 
 
 class MyTests(WebTest):
-	fixtures = ['starting.json']
+    fixtures = ['starting.json']
 
-	def test_of_model(self):
-		note = Note(title='sometitle', text='sometext')
-		note.save()
-		all_notes = Note.objects.all()
-		self.assertIn(note, all_notes)
+    def test_of_model(self):
+        note = Note(title='sometitle', text='sometext')
+        note.save()
+        all_notes = Note.objects.all()
+        self.assertIn(note, all_notes)
 
-	def test_of_note_output_at_index_page(self):
-		note = Note(title='sometitle', text='sometext')
-		note.save()
-		index_page = self.app.get('/')
-		assert note.title in index_page
-		assert note.text in index.page
+    def test_of_note_output_at_index_page(self):
+        note = Note(title='sometitle', text='sometext')
+        note.save()
+        index_page = self.app.get('/')
+        self.assertTemplateUsed(index_page, template_name='index.html')
+        assert note.title in index_page
+        assert note.text in index_page
 
-	def test_that_admin_works(self):
-		login_page = self.app.get('/admin')
-		form = login_page.form
-		form[u'username'] = 'nikita'
+    
+    def test_that_admin_works(self):
+        login_page = self.app.get('/admin')
+        form = login_page.form
+        form[u'username'] = 'nikita'
         form[u'password'] = 'n1k1ta'
         res = form.submit()
         res = res.follow()
@@ -30,4 +32,3 @@ class MyTests(WebTest):
         #I'm not really sure what particular link should I press
         res = res.click(href='note/add/')
         #and what will happen next.
-
